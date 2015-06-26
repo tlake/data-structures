a8244dde9ee3a7a97ba18890edcc031ca27851b8
tlake <tanner.lake@gmail.com>
2015-06-25 20:46:40 -0700

    created fixtures for stack tests

--- stack.py
+++ stack.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
 from linked_list import LinkedList
 
 
--- test_stack.py
+++ test_stack.py
@@ -1,37 +1,52 @@
 #!/usr/bin/env python
 
-
-from stack import Stack
+from __future__ import unicode_literals
 import pytest
+from stack import Stack
 
 
-def test_pop():
-    assert Stack([1, 2, 3]).pop() == 3
+@pytest.fixture()
+def create_stack():
+    return Stack([1, 2, 3])
 
 
-def test_pop_2():
-    assert Stack([1]).pop() == 1
+@pytest.fixture()
+def create_empty_stack():
+    return Stack()
 
 
-def test_push():
-    test_stack = Stack([1, 2, 3])
+@pytest.fixture
+def create_single_item_stack():
+    return Stack([1])
+
+
+def test_pop(create_stack):
+    assert create_stack.pop() == 3
+
+
+def test_pop_2(create_single_item_stack):
+    assert create_single_item_stack.pop() == 1
+
+
+def test_push(create_stack):
+    test_stack = create_stack
     test_stack.push(4)
     assert test_stack.group.head.val == 4
 
 
-def test_push_2():
-    test_stack = Stack([])
+def test_push_2(create_empty_stack):
+    test_stack = create_empty_stack
     test_stack.push(1)
     assert test_stack.group.head.val == 1
 
 
-def test_none():
-    test_stack = Stack()
+def test_none(create_empty_stack):
+    test_stack = create_empty_stack
     test_stack.push(2)
     assert test_stack.group.head.val == 2
 
 
-def test_pop_3():
+def test_pop_3(create_empty_stack):
     with pytest.raises(AttributeError):
-        test_stack = Stack()
+        test_stack = create_empty_stack
         test_stack.pop()
