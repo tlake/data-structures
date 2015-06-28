3788922aae608e40c3b37f97abc96c2415b0731c
Megan Slater <megan.e.slater@gmail.com>
2015-06-27 18:08:06 -0700

    made everything way simpler

--- parenthetics.py
+++ parenthetics.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python
 
 
-def parenthetics(str):
-    parens = []
-    for item in str:
-        parens.append(item)
+def parenthetics(str, symbol='()'):
     count = 0
-    open_parens = []
+    open_symbol, closed_symbol = symbol
     for item in str:
-        count += 1
-        if item == "(":
-            open_parens.append(count)
-    count_two = 0
-    closed_parens = []
-    for item in str:
-        count_two += 1
-        if item == ")":
-            closed_parens.append(count_two)
-    if len(closed_parens) < len(open_parens):
+        if item == "open_symbol":
+            count += 1
+        elif item == "closed_symbol":
+            count -= 1
+        if count == -1:
+            return count
+    if count == 0:
+        return count
+    if count > 0:
         return 1
-    count_final = 0
-    for number in range(len(open_parens) + 1):
-        if open_parens[count_final] > closed_parens[count_final]:
-            return -1
-            break
-        count += 1
-    return 0
