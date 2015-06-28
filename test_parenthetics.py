c38a7f780d25c8b008bd2da321ea3dc5f187a113
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-06-27 19:15:44 -0700

    Cleaned up tests

--- test_parenthetics.py
+++ test_parenthetics.py
@@ -3,119 +3,43 @@
 from parenthetics import parenthetics
 
 testText = "12345m67890!@#~`$%^&*/.,;'=-+_[]{}:?<>'"
+testSymbols = ['()', '{}', '[]', '<>', '5m']
 
 
-def test_too_many_open_paren():
-    assert parenthetics("(") == 1
-    assert parenthetics("((") == 1
-    assert parenthetics("(()") == 1
-    assert parenthetics("()(") == 1
-    assert parenthetics("()()()(") == 1
-    assert parenthetics("(adding(text((" + testText + ")works)too)") == 1
-    #  -------------
-    #  Other symbols
-    #  -------------
-
-    #  Square Brackets []
-    assert parenthetics("[", "[]") == 1
-    assert parenthetics("[[", "[]") == 1
-    assert parenthetics("[[]", "[]") == 1
-    assert parenthetics("[][", "[]") == 1
-    assert parenthetics("[][][][", "[]") == 1
-    assert parenthetics("[adding[text[[" + testText + "]works]too]", "[]") == 1
-
-    #  Curly Brackets {}
-    assert parenthetics("{", "{}") == 1
-    assert parenthetics("{{", "{}") == 1
-    assert parenthetics("{{}", "{}") == 1
-    assert parenthetics("{}{", "{}") == 1
-    assert parenthetics("{}{}{}{", "{}") == 1
-    assert parenthetics("{adding{text{{" + testText + "}works}too}", "{}") == 1
-
-    #  Angle Brackets <>
-    assert parenthetics("<", "<>") == 1
-    assert parenthetics("<<", "<>") == 1
-    assert parenthetics("<<<>", "<>") == 1
-    assert parenthetics("<<><", "<>") == 1
-    assert parenthetics("<<><<><<><", "<>") == 1
-    assert parenthetics("<adding<text<<" + testText + "<>works<>too<>", "<>") == 1
-
-    #  Arbitrary Symbols 5m
-    assert parenthetics("5", "5m") == 1
-    assert parenthetics("55", "5m") == 1
-    assert parenthetics("555m", "5m") == 1
-    assert parenthetics("55m5", "5m") == 1
-    assert parenthetics("55m55m55m5", "5m") == 1
-    assert parenthetics("5adding5text55" + testText + "5mworks5mtoo5m", "5m") == 1
+def test_extra_open():
+    for d in testSymbols:
+        o, c = d
+        assert parenthetics("{}".format(o), d) == 1
+        assert parenthetics("{}{}".format(o, o), d) == 1
+        assert parenthetics("{}{}{}".format(o, o, c), d) == 1
+        assert parenthetics("{}{}{}".format(o, c, o), d) == 1
+        assert parenthetics("{}{}{}{}{}{}{}".format(
+                            o, c, o, c, o, c, o), d) == 1
+        assert parenthetics("{}adding{}text{}{}".format(
+                            o, o, o, o) + testText + "{}works{}too{}".format(
+                            c, c, c), d) == 1
 
 
-def test_too_many_closed_paren():
-    assert parenthetics(")") == -1
-    assert parenthetics("))") == -1
-    assert parenthetics("())") == -1
-    assert parenthetics(")()") == -1
-    assert parenthetics("))") == -1
-    assert parenthetics("(adding(text(" + testText + ")(workstoo))))") == -1
-    #  -------------
-    #  Other symbols
-    #  -------------
-
-    #  Square Brackets []
-    assert parenthetics("]", "[]") == -1
-    assert parenthetics("]]", "[]") == -1
-    assert parenthetics("[]]", "[]") == -1
-    assert parenthetics("][]", "[]") == -1
-    assert parenthetics("]]", "[]") == -1
-    assert parenthetics("[adding[text[" + testText + "][workstoo]]]]", "[]") == -1
-    #  Curly Brackets {}
-    assert parenthetics("}", "{}") == -1
-    assert parenthetics("}}", "{}") == -1
-    assert parenthetics("{}}", "{}") == -1
-    assert parenthetics("}{}", "{}") == -1
-    assert parenthetics("}}", "{}") == -1
-    assert parenthetics("{adding{text{" + testText + "}{workstoo}}}}", "{}") == -1
-    #  Angle Brackets <>
-    assert parenthetics(">", "<>") == -1
-    assert parenthetics(">>", "<>") == -1
-    assert parenthetics("<>>", "<>") == -1
-    assert parenthetics("><>", "<>") == -1
-    assert parenthetics(">>", "<>") == -1
-    assert parenthetics("<adding<text<" + testText + "><workstoo>>>>", "<>") == -1
-    #  Arbitrary Symbols 5m
-    assert parenthetics("m", "5m") == -1
-    assert parenthetics("mm", "5m") == -1
-    assert parenthetics("5mm", "5m") == -1
-    assert parenthetics("m5m", "5m") == -1
-    assert parenthetics("mm", "5m") == -1
-    assert parenthetics("5adding5text5" + testText + "m5workstoommmm", "5m") == -1
+def test_extra_closed():
+    for d in testSymbols:
+        o, c = d
+        assert parenthetics("{}".format(c), d) == -1
+        assert parenthetics("{}{}".format(c, c), d) == -1
+        assert parenthetics("{}{}{}".format(o, c, c), d) == -1
+        assert parenthetics("{}{}{}".format(c, o, c), d) == -1
+        assert parenthetics("{}{}{}{}{}{}{}".format(
+                            o, c, o, c, o, c, c), d) == -1
+        assert parenthetics("{}adding{}text{}{}".format(
+                            o, o, o, c) + testText + "{}works{}too{}".format(
+                            c, c, c), d) == -1
 
 
-def test_correct_paren():
-    assert parenthetics("()") == 0
-    assert parenthetics("()()") == 0
-    assert parenthetics("(())") == 0
-    assert parenthetics("(adding(text(" + testText + ")works)too)") == 0
-    #  -------------
-    #  Other symbols
-    #  -------------
-
-    #  Square Brackets []
-    assert parenthetics("[]", "[]") == 0
-    assert parenthetics("[][]", "[]") == 0
-    assert parenthetics("[[]]", "[]") == 0
-    assert parenthetics("[adding[text[" + testText + "]works]too]", "[]") == 0
-    #  Curly Brackets {}
-    assert parenthetics("{}", "{}") == 0
-    assert parenthetics("{}{}", "{}") == 0
-    assert parenthetics("{{}}", "{}") == 0
-    assert parenthetics("{adding{text{" + testText + "}works}too}", "{}") == 0
-    #  Angle Brackets <>
-    assert parenthetics("<>", "<>") == 0
-    assert parenthetics("<><>", "<>") == 0
-    assert parenthetics("<<>>", "<>") == 0
-    assert parenthetics("<adding<text<" + testText + ">works>too>", "<>") == 0
-    #  Arbitrary Symbols 5m
-    assert parenthetics("5m", "5m") == 0
-    assert parenthetics("5m5m", "5m") == 0
-    assert parenthetics("55mm", "5m") == 0
-    assert parenthetics("5adding5text5" + testText + "mworksmtoom", "5m") == 0
+def test_no_extra():
+    for d in testSymbols:
+        o, c = d
+        assert parenthetics("{}{}".format(o, c), d) == 0
+        assert parenthetics("{}{}{}{}".format(o, c, o, c), d) == 0
+        assert parenthetics("{}{}{}{}".format(o, o, c, c), d) == 0
+        assert parenthetics("{}adding{}text{}{}".format(
+                            o, o, o, o) + testText + "{}works{}too{}{}".format(
+                            c, c, c, c), d) == 0
