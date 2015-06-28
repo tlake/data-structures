28d1a6e886e9e9b6514db01d48decec579f3a1d1
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-06-27 19:21:01 -0700

    fixed merge

--- parenthetics.py
+++ parenthetics.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 
 
-<<<<<<< HEAD
-def parenthetics2(str):
-    parens = []
-    for item in str:
-        parens.append(item)
-=======
-def parenthetics(str, symbol="()"):
-    open_symbol, closed_symbol = symbol
->>>>>>> 4a66c7df490e553fa0100f677fcd45e697c4f4bc
-    count = 0
-    for item in str:
-        if item == open_symbol:
-            count += 1
-        elif item == closed_symbol:
-            count -= 1
-    if count < 0:
-        return -1
-    if count > 0:
-        return 1
-<<<<<<< HEAD
-    count_final = 0
-    for number in range(len(open_parens) + 1):
-        if open_parens[count_final] > closed_parens[count_final]:
-            return -1
-            break
-        count += 1
-    return 0
+# <<<<<<< HEAD
+# def parenthetics2(str):
+#     parens = []
+#     for item in str:
+#         parens.append(item)
+# =======
+# def parenthetics(str, symbol="()"):
+#     open_symbol, closed_symbol = symbol
+# >>>>>>> 4a66c7df490e553fa0100f677fcd45e697c4f4bc
+#     count = 0
+#     for item in str:
+#         if item == open_symbol:
+#             count += 1
+#         elif item == closed_symbol:
+#             count -= 1
+#     if count < 0:
+#         return -1
+#     if count > 0:
+#         return 1
+# <<<<<<< HEAD
+#     count_final = 0
+#     for number in range(len(open_parens) + 1):
+#         if open_parens[count_final] > closed_parens[count_final]:
+#             return -1
+#             break
+#         count += 1
+#     return 0
 
 
 def parenthetics(str, symbol='()'):
@@ -45,7 +45,5 @@ def parenthetics(str, symbol='()'):
             return -1
     if open_count > closed_count:
         return 1
-=======
->>>>>>> 4a66c7df490e553fa0100f677fcd45e697c4f4bc
     else:
         return 0
