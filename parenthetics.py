182a997a9de0041b71a5ad41a062c874d86d1e82
tlake <tanner.lake@gmail.com>
2015-06-29 00:55:52 -0700

    now implemented with a stack data structure

--- parenthetics.py
+++ parenthetics.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 from __future__ import unicode_literals
+from stack import Stack
 
 
-def parenthetics(str, symbol='()'):
+def parenthetics(string, symbol='()'):
     open_symbol, closed_symbol = symbol
-    open_count, closed_count = 0, 0
-    for char in str:
+    stack = Stack()
+    for char in string:
         if char == open_symbol:
-            open_count += 1
-        elif char == closed_symbol:
-            closed_count += 1
-        if open_count < closed_count:
-            return -1
-    if open_count > closed_count:
+            stack.push(open_symbol)
+        if char == closed_symbol:
+            if stack.pop() == 0:
+                return -1
+    if stack.size() > 0:
         return 1
     else:
         return 0
