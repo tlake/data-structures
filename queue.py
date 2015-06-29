c288ce97e1166ff2fa3d1fb274ba8807fbb12c92
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-06-29 12:50:30 -0700

    Start of Queue

---
+++ queue.py
@@ -0,0 +1,7 @@
+"""
+enqueue(value): adds value to the queue
+dequeue(): removes the correct item from the queue and returns
+its value (should raise an error if the queue is empty) size():
+return the size of the queue.  Should return 0 if the queue is
+empty.
+"""
