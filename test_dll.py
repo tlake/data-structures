b65237e64e897142d0cce08dbdd12319bd69496e
tlake <tanner.lake@gmail.com>
2015-06-30 12:38:16 -0700

    wrote simple test for dll.insert()

---
+++ test_dll.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from doubly_linked_list import DoublyLinkedList
+import pytest
+
+
+@pytest.fixture()
+def create_dll():
+    dll = DoublyLinkedList([1, 2, 3])
+    return dll
+
+
+# insert(val) will insert the value 'val' at the head of the list
+def test_dll_insert(create_dll):
+    dll = create_dll.insert('insertion')
+    assert dll.head.val == 'insertion'
+
+
+# append(val) will append the value 'val' at the tail of the list
+
+
+# pop() will pop the first value off the head of the list and return it.
+
+
+# shift() will remove the last value from the tail of the list and return it.
+
+
+# remove(val) will remove the first instance of 'val' found in the list,
+# starting from the head. If 'val' is not present, it will raise an
+# appropriate Python exception.
