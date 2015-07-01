445d1916a5f12f6ea25867b6921fe08b2ab45ecd
Megan Slater <megan.e.slater@gmail.com>
2015-07-01 15:30:46 -0700

    add docstrings

--- test_binheap.py
+++ test_binheap.py
@@ -6,6 +6,9 @@ import pytest
 
 @pytest.fixture()
 def create_binheap():
+    """Creates a binary heap with an arbitrary list of numbers to use
+    for further testing.
+    """
     heap = BinaryHeap()
     heap.push(5)
     heap.push(13)
@@ -20,33 +23,41 @@ def create_binheap():
 
 @pytest.fixture()
 def create_empty_binheap():
+    """Creates an empty binary heap"""
     heap = BinaryHeap()
     return heap
 
 
-# push(val) will insert the value 'val' in the appropriate place
 def test_binheap_push(create_binheap):
+    """Insures that push inserts the value in the appropriate place
+    """
     binheap = create_binheap
     binheap.push(125)
     assert binheap.heap[0] == 125
     binheap.push(4)
     assert binheap.heap[0] == 125
-    assert binheap.heap[8] == 4
+    assert binheap.heap[9] == 4
 
 
 def test_binheap_length(create_binheap):
+    """Makes sure that all the values are being added to the
+    binary heap.
+    """
     binheap = create_binheap
     assert len(binheap.heap) == 8
 
 
 def test_binheap_push_when_empty(create_empty_binheap):
+    """Ensures the heap can accept values when empty"""
     binheap = create_empty_binheap
     binheap.push(125)
     assert binheap.heap[0] == 125
 
 
-# pop(val) will pop the value 'val' from the top
 def test_binheap_pop(create_binheap):
+    """pop(val) will pop the value from the top and rearrange
+    all other values
+    """
     binheap = create_binheap
     binheap.pop()
     assert binheap.heap[0] == 85
@@ -55,12 +66,9 @@ def test_binheap_pop(create_binheap):
 
 
 def test_binheap_pop_when_empty(create_empty_binheap):
+    """Ensures that an exception is raised when trying
+    to remove anything from an empty heap.
+    """
     with pytest.raises(Exception):
         binheap = create_empty_binheap
         binheap.pop("this heap is empty")
-
-
-def test_max_heapify_empty_heap(create_empty_binheap):
-    with pytest.raises(Exception):
-        binheap = create_empty_binheap
-        binheap.max_heapify("this heap is empty")
