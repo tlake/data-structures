b43eb3e7e373786c91c1e993670379e497269fc4
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-07-01 15:55:44 -0700

    modified tests

--- test_binheap.py
+++ test_binheap.py
@@ -9,15 +9,7 @@ def create_binheap():
     """Creates a binary heap with an arbitrary list of numbers to use
     for further testing.
     """
-    heap = BinaryHeap()
-    heap.push(5)
-    heap.push(13)
-    heap.push(2)
-    heap.push(85)
-    heap.push(106)
-    heap.push(42)
-    heap.push(3)
-    heap.push(25)
+    heap = BinaryHeap([15, 13, 2, 85, 106, 42, 3, 25])
     return heap
 
 
@@ -36,15 +28,17 @@ def test_binheap_push(create_binheap):
     assert binheap.heap[0] == 125
     binheap.push(4)
     assert binheap.heap[0] == 125
-    assert binheap.heap[9] == 4
+    assert binheap.heap[binheap.size-1] == 4
 
 
-def test_binheap_length(create_binheap):
+def test_binheap_length(create_binheap, create_empty_binheap):
     """Makes sure that all the values are being added to the
     binary heap.
     """
+    binheap = create_empty_binheap
+    assert create_empty_binheap.size == 0
     binheap = create_binheap
-    assert len(binheap.heap) == 8
+    assert binheap.size == 8
 
 
 def test_binheap_push_when_empty(create_empty_binheap):
@@ -59,16 +53,27 @@ def test_binheap_pop(create_binheap):
     all other values
     """
     binheap = create_binheap
+    assert binheap.heap[0] == 106
     binheap.pop()
     assert binheap.heap[0] == 85
     binheap.pop()
     assert binheap.heap[0] == 42
+    binheap.pop()
+    assert binheap.heap[0] == 25
+    binheap.pop()
+    assert binheap.heap[0] == 15
+    binheap.pop()
+    assert binheap.heap[0] == 13
+    binheap.pop()
+    assert binheap.heap[0] == 3
+    binheap.pop()
+    assert binheap.heap[0] == 2
 
 
 def test_binheap_pop_when_empty(create_empty_binheap):
     """Ensures that an exception is raised when trying
     to remove anything from an empty heap.
     """
-    with pytest.raises(Exception):
+    with pytest.raises(IndexError):
         binheap = create_empty_binheap
-        binheap.pop("this heap is empty")
+        binheap.pop()
