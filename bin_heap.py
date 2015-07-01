57891d426af5ee25b43e994b27043f58add78cec
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-07-01 15:19:27 -0700

    fixed push

--- bin_heap.py
+++ bin_heap.py
@@ -14,11 +14,19 @@ class BinaryHeap(object):
     def push(self, val):
         self.heap.append(val)
         self.size += 1
-        #  do fun stuff
+        self.max_heapify2(self.size-1)
+
+    def max_heapify2(self, child):
+        if child != 0:
+            parent = (child - 1) // 2
+            if self.heap[parent] < self.heap[child]:
+                self.swap(child, parent)
+                self.max_heapify2(parent)
 
     def pop(self):
         tmp = self.heap[0]
         self.heap[0] = self.heap.pop()
+        self.size -= 1
         self.max_heapify(0)
         return tmp
 
@@ -27,15 +35,16 @@ class BinaryHeap(object):
         # 2i + 1 and 2i + 2
         # A child at index i finds its parent's index with:
         #  (i - 1) // 2
+        #import pdb; pdb.set_trace()
         left = 2 * index + 1
         right = 2 * index + 2
         largest = index
         #  make sure left is not out of bounds, then
         #  make sure left is not bigger than the biggest node
-        if left < self.size and self.heap[left] > self.heap[index]:
+        if left < self.size and self.heap[left] > self.heap[largest]:
             largest = right
         #  make sure right is not bigger than the biggest node
-        if right < self.size and self.heap[right] > self.heap[index]:
+        if right < self.size and self.heap[right] > self.heap[largest]:
             largest = left
         #  make the swap
         if largest != index:
