771cef8a50b964fafa974471fa986d2e87995810
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-07-02 15:08:51 -0700

    Added insert tests

--- test_priorityq.py
+++ test_priorityq.py
@@ -1,91 +1,126 @@
-#
-#
-# Will change lots, this is just a copy and paste
-#
-#
-
-
-
-
-
-
-
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from bin_heap import BinaryHeap
+from priorityq import PriorityQueue
 import pytest
 
 
 @pytest.fixture()
-def create_binheap():
-    """Creates a binary heap with an arbitrary list of numbers to use
+def create_pq():
+    """Creates a priotity queue with an arbitrary list of numbers to use
     for further testing.
     """
-    heap = BinaryHeap([15, 13, 2, 85, 106, 42, 3, 25])
-    return heap
+    pq = PriorityQueue([15, 13, 2, 85, 106, 42, 3, 25])
+    return pq
 
 
 @pytest.fixture()
-def create_empty_binheap():
-    """Creates an empty binary heap"""
-    heap = BinaryHeap()
-    return heap
-
-
-def test_binheap_push(create_binheap):
-    """Insures that push inserts the value in the appropriate place
+def create_pq2():
+    """Creates a priotity queue with an arbitrary list of numbers to use
+    for further testing.
     """
-    binheap = create_binheap
-    binheap.push(125)
-    assert binheap.heap[0] == 125
-    binheap.push(4)
-    assert binheap.heap[0] == 125
-    assert binheap.heap[binheap.size-1] == 4
+    pq = PriorityQueue()
+    pq.insert((1, "We"))
+    pq.insert((2, "eee"))
+    pq.insert((1, "eee"))
+    pq.insert((3, "are"))
+    pq.insert((1, "the"))
+    pq.insert((4, "champions"))
+    pq.insert((1, "my"))
+    pq.insert((5, "friends"))
+    return pq
 
 
-def test_binheap_length(create_binheap, create_empty_binheap):
-    """Makes sure that all the values are being added to the
-    binary heap.
+@pytest.fixture()
+def create_empty_pq():
+    """Creates an empty priotity queue"""
+    pq = PriorityQueue()
+    return pq
+
+#
+#  Test Insert
+#
+def test_pq_insert1(create_pq):
+    """Insures that insert inserts the value in the appropriate place
     """
-    binheap = create_empty_binheap
-    assert create_empty_binheap.size == 0
-    binheap = create_binheap
-    assert binheap.size == 8
+    pq = create_pq
+    #  If you add an item with the highest priority,
+    #  It will always be at the top of the list.
+    pq.insert((101, "Test1"))
+    assert pq.heap[0].value == "Test1"
+    pq.insert((102, "Test2"))
+    assert pq.heap[0].value == "Test2"
+    pq.insert((103, "Test3"))
+    assert pq.heap[0].value == "Test3"
+
+    #  If you add an item with the lowest priority,
+    #  It will always be at the bottom of the list.
+    pq.insert((104, "Test4"))
+    assert pq.heap[0].value == "Test4"
+    pq.insert((105, "Test5"))
+    assert pq.heap[0].value == "Test5"
+    pq.insert((106, "Test6"))
+    assert pq.heap[0].value == "Test6"
 
 
-def test_binheap_push_when_empty(create_empty_binheap):
-    """Ensures the heap can accept values when empty"""
-    binheap = create_empty_binheap
-    binheap.push(125)
-    assert binheap.heap[0] == 125
-
-
-def test_binheap_pop(create_binheap):
+def test_pq_insert2(create_pq):
     """pop(val) will pop the value from the top and rearrange
     all other values
     """
-    binheap = create_binheap
-    assert binheap.heap[0] == 106
-    binheap.pop()
-    assert binheap.heap[0] == 85
-    binheap.pop()
-    assert binheap.heap[0] == 42
-    binheap.pop()
-    assert binheap.heap[0] == 25
-    binheap.pop()
-    assert binheap.heap[0] == 15
-    binheap.pop()
-    assert binheap.heap[0] == 13
-    binheap.pop()
-    assert binheap.heap[0] == 3
-    binheap.pop()
-    assert binheap.heap[0] == 2
+    pq = PriorityQueue()
+    pq.insert((1, "We"))
+    assert pq.heap[0] == "We"
+    pq.insert((2, "eee"))
+    assert pq.heap[0] == "eee"
+    pq.insert((1, "eee"))
+    assert pq.heap[0] == "eee"
+    pq.insert((3, "are"))
+    assert pq.heap[0] == "are"
+    pq.insert((1, "the"))
+    assert pq.heap[0] == "are"
+    pq.insert((4, "champions"))
+    assert pq.heap[0] == "champions"
+    pq.insert((1, "my"))
+    assert pq.heap[0] == "champions"
+    pq.insert((5, "friends"))
+    assert pq.heap[0] == "friends"
 
 
-def test_binheap_pop_when_empty(create_empty_binheap):
+def test_pq_length(create_pq, create_empty_pq):
+    """Makes sure that all the values are being added to the
+    priotity queue.
+    """
+    pq = create_empty_pq
+    assert len(pq.heap) == 0
+    pq = create_pq
+    assert len(pq.heap) == 8
+
+
+def test_pq_pop(create_pq):
+    """pop(val) will pop the value from the top and rearrange
+    all other values to maintain the priority queue/max heap property
+    """
+    pq = create_pq
+    assert pq.heap[0] == "friends"
+    pq.pop()
+    assert pq.heap[0] == "champions"
+    pq.pop()
+    assert pq.heap[0] == "are"
+    pq.pop()
+    assert pq.heap[0] == "eee"
+    pq.pop()
+    assert pq.heap[0] == "We"
+    pq.pop()
+    assert pq.heap[0] == "eee"
+    pq.pop()
+    assert pq.heap[0] == "the"
+    pq.pop()
+    assert pq.heap[0] == "my"
+
+
+def test_pq_pop_when_empty(create_empty_pq):
     """Ensures that an exception is raised when trying
-    to remove anything from an empty heap.
+    to pop() from an empty heap.
     """
     with pytest.raises(IndexError):
-        binheap = create_empty_binheap
-        binheap.pop()
+        pq = create_empty_pq
+        pq.pop()
