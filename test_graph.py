850e2bc28ef6cac98b8a62e4a6e13500215daaad
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-07-06 16:43:09 -0700

    more skeletoning of test_graph

--- test_graph.py
+++ test_graph.py
@@ -20,29 +20,39 @@ def test_edges(create_graph):
     #  assert edges == ???
 
 
-def test_add_node():
-    pass
+def test_add_node(create_graph):
+    graph = create_graph
+    #  graph.add_node()#  not sure what we are passing in yet
 
 
-def test_add_edge():
-    pass
+def test_add_edge(create_graph):
+    graph = create_graph
+    #  graph.add_edge()#  not sure what we are passing in yet
 
 
-def test_del_node():
-    pass
+def test_del_node(create_graph):
+    graph = create_graph
+    #  we need to make/find a node here
+    #  graph.del_node()#  not sure what we are passing in yet
 
 
-def test_del_edge():
-    pass
+def test_del_edge(create_graph):
+    graph = create_graph
+    #  graph.del_edge()#  not sure what we are passing in yet
 
 
-def test_has_node():
-    pass
+def test_has_node(create_graph):
+    graph = create_graph
+    #  graph.has_node()#  not sure what we are passing in yet
 
 
-def test_neighbors():
-    pass
+def test_neighbors(create_graph):
+    graph = create_graph
+    #  we need to make/find a node here
+    #  graph.has_node()#  not sure what we are passing in yet
 
 
-def test_adjacent():
-    pass
+def test_adjacent(create_graph):
+    graph = create_graph
+    #  we need to make/find 2 nodes here
+    # graph.adjacent(node1, node2)
