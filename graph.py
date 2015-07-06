a72e2e04a43aae3f0516c7cbf6deb15e9e8591fd
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-07-06 16:10:52 -0700

    skeleton of graph.py

---
+++ graph.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals
+
+class Graph(object):
+    def __init__(self):
+        pass
+
+    def nodes(self):
+        """Returns a list of all nodes in the graph."""
+        pass
+
+    def edges(self):
+      """Returns a list of all edges in the graph."""
+        pass
+
+    def add_node(self, new_node):
+        """Adds a new node to the graph"""
+        pass
+
+    def add_edge(self, node1, node2):
+        """Adds a new edge to the graph connecting node1 to node2.
+        If either node1 or node2 are not already present in the
+        graph, they are added."""
+        pass
+
+    def del_node(self, node):
+        """Deletes the node from the graph. Raises an error if no
+        such node exists"""
+        pass
+
+    def del_edge(self, node1, node2):
+        """Deletes the edge connecting node1 and node2 from the graph.
+        Raises an error if no such edge exists."""
+        pass
+
+    def has_node(self, node):
+        """True if node is in the graph, False if it is not."""
+        pass
+
+    def neighbors(self, node):
+        """Returns a list of all nodes connected to node by edges.
+        Raises an error if node is not in the graph."""
+        pass
+
+    def adjacent(node1, node2):
+        """Returns True if there is an edge connecting node1 and node2.
+        False if no such edge exists. Raises an error if either of the
+        supplied nodes are not in the graph."""
+        pass
+
