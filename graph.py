b5548952537df23e8cba964a386b69209a69054a
Megan Slater <megan.e.slater@gmail.com>
2015-07-07 12:23:34 -0700

    add node, edges, nodes

--- graph.py
+++ graph.py
@@ -2,21 +2,29 @@
 
 from __future__ import unicode_literals
 
+
 class Graph(object):
     def __init__(self):
-        pass
+        self.graph = {}
 
     def nodes(self):
         """Returns a list of all nodes in the graph."""
-        pass
+        keys = []
+        for key in self.graph.iterkeys():
+            keys.append(key)
+        return keys
 
     def edges(self):
-      """Returns a list of all edges in the graph."""
-        pass
+        """Returns a list of all edges in the graph."""
+        nodes = self.nodes()
+        edges = []
+        for node in nodes:
+            edges.append(self.graph[node])
+        return edges
 
     def add_node(self, new_node):
         """Adds a new node to the graph"""
-        pass
+        self.graph[new_node] = []
 
     def add_edge(self, node1, node2):
         """Adds a new edge to the graph connecting node1 to node2.
