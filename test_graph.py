dcd88629c806190d8e88523b473cd91c33ec5d87
Megan Slater <megan.e.slater@gmail.com>
2015-07-07 13:19:41 -0700

    add tests for delete node, delete edge, add edge

--- test_graph.py
+++ test_graph.py
@@ -39,19 +39,39 @@ def test_edges(create_graph):
 
 
 def test_add_edge(create_graph):
+    """asserts that the edges we add are returned
+    and that if a new node is added as an edge that
+    node is added to the graph.
+    """
     graph = create_graph
     graph.add_edge(5, 1)
+    graph.add_edge(5, 6)
+    edge_one = (5, 1)
+    edge_two = (5, 6)
+    assert edge_one in graph.edges()
+    assert edge_two in graph.edges()
+    assert 6 in graph.nodes()
 
 
 def test_del_node(create_graph):
+    """deletes an existing node and asserts that the node is
+    no longer there and that its associated edges are also
+    no longer there
+    """
     graph = create_graph
-    #  we need to make/find a node here
-    #  graph.del_node()#  not sure what we are passing in yet
+    graph.del_node(1)
+    assert 1 not in graph.nodes()
+    assert (1, 2) not in graph.edges()
 
 
 def test_del_edge(create_graph):
+    """deletes an existing edge and make sure that the edge
+    no longer exists but that the node itself still does.
+    """
     graph = create_graph
-    #  graph.del_edge()#  not sure what we are passing in yet
+    graph.del_edge(1, 2)
+    assert (1, 2) not in graph.edges()
+    assert 1 in graph.nodes()
 
 
 def test_has_node(create_graph):
