b3ad52d92d4b88835ff970b538ca2bacb13b2045
tlake <tanner.lake@gmail.com>
2015-07-09 11:17:55 -0700

    wrote setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+long_description = """
+This is a package that provides some basic data structures
+implemented in Python.
+"""
+
+
+setup(
+    name="data-structures",
+    version="0.1-dev",
+    description="Basic Data Structures",
+    long_description=long_description,
+    # The project URL.
+    url='http://github.com/<yourname>/data-structures',
+    # Author details
+    author='<Your Name>',
+    author_email='<your.email@domain.com',
+    # Choose your license
+    #   and remember to include the license text in a 'docs' directory.
+    # license='MIT',
+    packages=['data_structures'],
+    install_requires=['setuptools', ],
+    extra_requires={
+        'tests': ['pytest', ],
+    }
+)
